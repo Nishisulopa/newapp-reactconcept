017149b74f223723a213736f70c49127826befaf
Nishisulopa <nishikanta.sahoo@sulopa.com>
2024-12-01 10:17:18 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
     <>
       {/* <Navbar /> */}
       {/* <FirstPage /> */}
-      {/* <CardShow /> */}
-      <FaqSection />
+      <CardShow />
+      {/* <FaqSection /> */}
     </>
   );
 }
